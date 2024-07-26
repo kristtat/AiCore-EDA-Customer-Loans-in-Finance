bfb4fe7a9f65f6520a6c94154180bfdf8e400dce
kristtat <133046403+kristtat@users.noreply.github.com>
2024-07-26 13:53:38 +0100

    Final additions before submission.

--- datatransformation.py
+++ datatransformation.py
@@ -7,10 +7,7 @@ class DataTransform:
 
     Methods:
         __init__(self, df):
-            Initializes the DataTransform object with a given DataFrame.
-
-        convert_float_to_int(self, columns):
-            Converts specified columns to integer type after filling NaN values with 0.
+            Initialises the DataTransform object with a given DataFrame.
 
         convert_mixed_obj_to_int(self, columns):
             Converts specified columns containing mixed data types to integer after coercing to numeric type.
@@ -20,6 +17,9 @@ class DataTransform:
 
         convert_to_categorical(self, columns):
             Converts specified columns to categorical type.
+            
+        apply_transformations(self):
+            Applies all transformations defined for the DataFrame.
 
     """
 
@@ -36,44 +36,29 @@ class DataTransform:
 
         self.df = df
     
-    def convert_float_to_int(self, columns):
-
-        """
-
-        Converts columns to integer type after filling NaN values with 0.
-
-        Parameters:
-            columns (list): List of column names to convert.
-
-        Returns:
-            pandas.DataFrame: DataFrame with specified columns converted to integer type.
-
-        """
-
-        for col in columns:
-            self.df[col] = self.df[col].fillna(0).astype(int)
-        print("All relevant columns converted to integer type.")
-        return self.df
 
     def convert_mixed_obj_to_int(self, columns):
 
         """
-        
-        Converts specified columns containing mixed data types to integer after coercing to numeric type and fills NaNs as 0.
+        Converts specified columns containing mixed data types to integer after extracting numeric part.
+        Fills NaNs with 0.
 
         Parameters:
             columns (list): List of column names to convert.
 
         Returns:
-            pandas.DataFrame: DataFrame with specified columns converted to integer type.
+            pandas.DataFrame: DataFrame with specified columns converted to integer type. 
 
-        
         """
-
+        
         for col in columns:
-            self.df[col] = pd.to_numeric(self.df[col], errors='coerce').fillna(0).astype(int)
-        print("All relevant columns converted to integer type.")
+            try:
+                self.df[col] = self.df[col].str.extract('(\d+)').fillna(0).astype(int)
+                print(f"Converted column '{col}' to integer format.")
+            except Exception as e:
+                print(f"Error converting column '{col}': {e}")
         return self.df
+        
 
     def convert_date_object_to_datetime(self, columns):
 
@@ -88,9 +73,10 @@ class DataTransform:
             pandas.DataFrame: DataFrame with specified columns converted to datetime format.
         
         """
+        date_format = "%b-%Y"
         for col in columns:
-            self.df[col] = pd.to_datetime(self.df[col], format='%b-%Y')
-        print("All relevant columns converted to datetime type.")
+                self.df[col] = pd.to_datetime(self.df[col], format=date_format, errors='coerce')
+                print(f"Converted column '{col}' to datetime format.")
         return self.df
 
     def convert_to_categorical(self, columns):
@@ -107,9 +93,31 @@ class DataTransform:
         
         """
         for col in columns:
-            self.df[col] = self.df[col].astype('category')
-        print("All relevant columns converted to categorical type.")
+            try:
+                self.df[col] = self.df[col].astype('category')
+                print(f"Converted column '{col}' to categorical format.")
+            except Exception as e:
+                print(f"Error converting column '{col}': {e}")
         return self.df
+        
+
+    def apply_transformations(self):
+
+        """
+
+        Applies all transformations defined for the DataFrame.
 
 
- 
+        """
+       
+        columns_to_transform_mixed_obj_to_int = ['term', 'employment_length']
+        columns_to_transform_date_object_to_datetime = ['issue_date', 'earliest_credit_line', 'last_payment_date', 'last_credit_pull_date']
+        columns_to_transform_to_categorical = ['grade', 'sub_grade', 'home_ownership', 'verification_status', 'loan_status', 'payment_plan','purpose','policy_code', 'application_type']
+
+      
+        self.convert_mixed_obj_to_int(columns_to_transform_mixed_obj_to_int)
+        self.convert_date_object_to_datetime(columns_to_transform_date_object_to_datetime)
+        self.convert_to_categorical(columns_to_transform_to_categorical)
+
+        print("All transformations successfully completed.")
+        return self.df
